6dde30a5e150b8185845d8fe4c0a0355d2756924
Alessandra747 <sandrapzr@hotmail.com>
2022-07-08 17:58:51 -0300

    Ingles1Mod2

---
+++ script.js
@@ -0,0 +1,37 @@
+//Crie 3 variáveis, cada uma com um array:
+
+//Dentro de cada array, insira 3 (TRÊS) Objetos, contendo:
+
+****************************************
+
+let comedy = [
+    {
+      title: "Fre Guy",
+      director: "Shawn Levy",
+      year: 2021,
+    },
+  ]
+  console.log("Film Comedy:", comedy);
+  
+  let action = [
+    {
+      title: "Morbius",
+      director: "Daniel Espinosa",
+      year: 2022,
+    },
+  ]
+  
+  console.log("Film Action:", action);
+  
+  let fantasy = [
+    {
+      title: "Jujutsu Kaisen 0",
+      director: "Sunghoo Park",
+      year: 2021,
+    },
+  ]
+  
+  console.log("Film Fantasy:", fantasy);
+  
+
+
