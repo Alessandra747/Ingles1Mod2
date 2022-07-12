0d75b4b26afae8b531be7b8541e356756c08dbca
Alessandra747 <sandrapzr@hotmail.com>
2022-07-12 15:19:37 -0300

    DesaioInglesMod2

--- script.js
+++ script.js
@@ -4,33 +4,25 @@
 
 ****************************************
 
-let comedy = [
-    {
+let comedy = [{
       title: "Fre Guy",
       director: "Shawn Levy",
       year: 2021,
-    },
-  ]
+    }]
   console.log("Film Comedy:", comedy);
   
-  let action = [
-    {
+  let action = [{
       title: "Morbius",
       director: "Daniel Espinosa",
       year: 2022,
-    },
-  ]
-  
+    }]
   console.log("Film Action:", action);
   
-  let fantasy = [
-    {
+  let fantasy = [{
       title: "Jujutsu Kaisen 0",
       director: "Sunghoo Park",
       year: 2021,
-    },
-  ]
-  
+    }]
   console.log("Film Fantasy:", fantasy);
   
 
